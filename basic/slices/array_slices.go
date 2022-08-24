eebd381ccb7b803c036d1d456687568621716169
ceezyyy <ceezyyy11@gmail.com>
2022-08-24 21:03:57 +0800

    style: remove

--- basic/slices/array_slices.go
+++ basic/slices/array_slices.go
@@ -2,58 +2,24 @@ package main
 
 import "fmt"
 
-type KAWechatInfo struct {
-	DmError  int    `json:"dm_error"`
-	ErrorMsg string `json:"error_msg"`
-	Data     struct {
-		KaStatus      int         `json:"ka_status"`
-		KaLevel       int         `json:"ka_level"`
-		ImgType       int         `json:"img_type"`
-		Title         string      `json:"title"`
-		Subtitle      string      `json:"subtitle"`
-		Code          string      `json:"code"`
-		H5URL         string      `json:"h5_url"`
-		ImageURL      string      `json:"image_url"`
-		BannerURL     string      `json:"banner_url"`
-		Message       string      `json:"message"`
-		RawCode       string      `json:"raw_code"`
-		OfficialCode  string      `json:"official_code"`
-		ImageMediaID  string      `json:"image_media_id"`
-		WelfwareTitle string      `json:"welfware_title"`
-		Welfware      interface{} `json:"welfware"`
-		InstantTitle  string      `json:"instant_title"`
-		Jump          struct {
-			Path string `json:"path"`
-			ID   string `json:"id"`
-			Type int    `json:"type"`
-		} `json:"jump"`
-		TaskCenter struct {
-			Img  string `json:"img"`
-			Text string `json:"text"`
-		} `json:"task_center"`
-		InstantAward interface{} `json:"instant_award"`
-		AddWay       int         `json:"add_way"`
-	} `json:"data"`
-}
-
 func main() {
 
-	//var arr1 [5]int
-	//var slice1 = arr1[1:3]
-	//
-	//// init
-	//for i := 0; i < len(arr1); i++ {
-	//	arr1[i] = i
-	//}
-	//
-	//// slice1
-	//for i := 0; i < len(slice1); i++ {
-	//	fmt.Println(slice1[i])
-	//}
-	//
-	//fmt.Printf("len(arr1): %d\n", len(arr1))
-	//fmt.Printf("len(slice1): %d\n", len(slice1))
-	//fmt.Printf("cap(slice1): %d\n", cap(slice1))
+	var arr1 [5]int
+	var slice1 = arr1[1:3]
+
+	// init
+	for i := 0; i < len(arr1); i++ {
+		arr1[i] = i
+	}
+
+	// slice1
+	for i := 0; i < len(slice1); i++ {
+		fmt.Println(slice1[i])
+	}
+
+	fmt.Printf("len(arr1): %d\n", len(arr1))
+	fmt.Printf("len(slice1): %d\n", len(slice1))
+	fmt.Printf("cap(slice1): %d\n", cap(slice1))
 
 	sl := make([]int, 2)
 	fmt.Println(sl)
@@ -65,8 +31,4 @@ func main() {
 	}
 
 	fmt.Println(sl)
-
-	var res KAWechatInfo
-	fmt.Printf("%+v\n", res)
-
 }
